dc1c29d0441a880002d826a2d620aa24cd8ba2f7
yuecpyue <a0985837774@gmail.com>
2023-11-14 10:33:30 +0800

    week10

---
+++ week10/week10-4.py
@@ -0,0 +1,17 @@
+a=[9,8,7,6,5,4,3,2,1,0]
+N=len(a)#取出陣列的長度
+print(a)
+for i in range(N-1):#只做一輪的泡泡
+  if a[i+1]<a[i]:
+    a[i],a[i+1]=a[i+1],a[i]
+print(a)
+
+
+a=[9,8,7,6,5,4,3,2,1,0]
+N=len(a)#取出陣列的長度
+print(a)
+for k in range(N):#做很多次
+  for i in range(N-1):#只做一輪的泡泡
+    if a[i+1]<a[i]:
+      a[i],a[i+1]=a[i+1],a[i]
+print(a)
